8ac2ecd1edd7cdd21385f4e44bee5155886507eb
shadowczp <czp5153409@qq.com>
2018-05-04 16:43:23 +0800

    demo

--- cloud/src/main/java/com/example/cloud/CloudApplication.java
+++ cloud/src/main/java/com/example/cloud/CloudApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class CloudApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CloudApplication.class, args);
 	}
 }
