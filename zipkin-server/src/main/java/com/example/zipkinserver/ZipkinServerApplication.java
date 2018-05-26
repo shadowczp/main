0b2874b40e775b9addec8c1923c6d66edb04fcec
shadowczp <695034851@qq.com>
2018-05-27 01:03:22 +0800

    20180527

---
+++ zipkin-server/src/main/java/com/example/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
