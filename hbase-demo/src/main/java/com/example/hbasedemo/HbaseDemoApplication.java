7a1457372c4dad76c4755a2d7b562b2410245263
shadowczp <czp5153409@qq.com>
2018-09-10 22:25:14 +0800

    hbase客户端demo

---
+++ hbase-demo/src/main/java/com/example/hbasedemo/HbaseDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.hbasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbaseDemoApplication.class, args);
+    }
+}
